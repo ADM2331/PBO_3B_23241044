bf8490310ba2100b290ba7ceea5fe7508768f197
ADM2331 <146051140+ADM2331@users.noreply.github.com>
2024-10-08 11:56:08 +0800

    Add files via upload

---
+++ PBO/PRE-TEST/ProgramKalkulator/src/App.java
@@ -0,0 +1,15 @@
+// membuat class
+class PerogramKalkulator{
+
+    //Mengisi Atribut Kalkulator
+    double datadouble;
+}
+
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
